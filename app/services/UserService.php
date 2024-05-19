c33cb8e66a6c468007e07d1d13f630adf91b0cb0
jet2018 <ezrajet9@gmail.com>
2024-05-19 17:15:41 +0300

    releasing v1.0.5

--- app/services/UserService.php
+++ app/services/UserService.php
@@ -8,8 +8,17 @@ use Pionia\response\BaseResponse;
 
 class UserService extends BaseRestService
 {
+
      protected function login($data, $files): BaseResponse
     {
+        $email = $data['email'];
+        $password = $data['password'];
+
+        // these are validations here
+        $this->asEmail($email);
+        $this->asPassword($password);
+
+
        return BaseResponse::JsonResponse(0, "We reached well", [$data,$files]);
     }
 
