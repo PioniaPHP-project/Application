421b7dff8ae098b7f3621c22f0703708875d04de
jet2018 <ezrajet9@gmail.com>
2026-07-02 17:58:38 +0300

    Introduces the new Pionia from how we run it to how we serve it in production.
    Cleaned out all the bugs in version 2

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Pionia\Autoload\ApplicationAutoloader;
 use Pionia\Realm\AppRealm;
+
+ApplicationAutoloader::register(dirname(__DIR__));
 
 return AppRealm::create(__DIR__);
